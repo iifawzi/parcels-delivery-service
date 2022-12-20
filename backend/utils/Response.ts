b689714fc3236c88a8dcdedc53bee73e98624690
iifawzi <iifawzie@gmail.com>
2022-12-20 19:51:23 +0200

    Adding response utility
    Signed-off-by: iifawzi <iifawzie@gmail.com>

---
+++ backend/utils/Response.ts
@@ -0,0 +1,19 @@
+import { Response } from "express";
+
+export class ResponseUtility {
+    private static respondWith = (status: boolean, message: string, statusCode: number, data: any, res: Response) => {
+        res.status(statusCode).json({
+            status,
+            message,
+            data,
+        });
+    };
+
+    public static Success(statusCode: number, data: any, res: Response) {
+        this.respondWith(true, 'success', statusCode, data, res)
+    }
+
+    public static Failure(message: string, statusCode: number, data: any, res: Response) {
+        this.respondWith(false, message, statusCode, data, res)
+    }
+}
