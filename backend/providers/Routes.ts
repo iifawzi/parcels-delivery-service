893966069b5070ed08851c1856e1ede6a73967c3
iifawzi <iifawzie@gmail.com>
2022-12-20 19:49:45 +0200

    Adding the routes component
    Signed-off-by: iifawzi <iifawzie@gmail.com>

--- backend/providers/Routes.ts
+++ backend/providers/Routes.ts
@@ -1,21 +1,22 @@
 import { Application, NextFunction, Request, Response } from 'express';
 import bodyParser from 'body-parser';
-import { BaseError } from './ErrorHandler';
-
+import router from '../routes';
 
 class Routes {
     public static mountRoutes(express: Application): Application {
         const apiPrefix = express.locals.app.apiPrefix;
+
+        // Body Parser Settings: 
         express.use(bodyParser.json());
+        // Required Headers: 
         express.use((req: Request, res: Response, next: NextFunction) => {
             res.setHeader("Access-Control-Allow-Origin", "*");
             res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH, DELETE");
             res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
             next();
         });
-        return express.use(`/${apiPrefix}/test`, (req: any, res: any, next: any) => {
-            throw new BaseError(500,"testing");
-        });
+
+        return express.use(apiPrefix, router);
     }
 }
 
