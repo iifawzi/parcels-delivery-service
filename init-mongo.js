ffbdf23905b713f67d023ce8fa2999b518fa021a
iifawzi <iifawzie@gmail.com>
2022-12-22 02:27:36 +0200

    update gitignore and init-mongo script
    Signed-off-by: iifawzi <iifawzie@gmail.com>

--- init-mongo.js
+++ init-mongo.js
@@ -94,3 +94,102 @@ db.customers.insertMany(
   }
   ],
 );
+
+// TESTING Configs: 
+db = db.getSiblingDB('test-saloodo')
+
+db.createUser({
+  user: 'testSaloodoUser',
+  pwd: 'testSaloodoPass',
+  roles: [
+    {
+      role: 'readWrite',
+      db: 'test-saloodo',
+    },
+  ],
+});
+
+db.bikers.insertMany(
+  [{
+    username: "biker1",
+    _id: new ObjectId("63a22b00a704bee4b0254f4c"),
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Biker Number 1"
+  },
+  {
+    username: "biker2",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Biker Number 2"
+  },
+  {
+    username: "biker3",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Biker Number 3"
+  },
+  {
+    username: "biker4",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Biker Number 4"
+  },
+  {
+    username: "biker5",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Biker Number 5"
+  },
+  {
+    username: "biker6",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Biker Number 6"
+  },
+  {
+    username: "biker7",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Biker Number 7"
+  },
+  {
+    username: "biker8",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Biker Number 8"
+  },
+  {
+    username: "biker9",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Biker Number 9"
+  },
+  {
+    username: "biker10",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Biker Number 10"
+  }
+  ],
+);
+
+db.customers.insertMany(
+  [{
+    username: "customer1",
+    _id: new ObjectId("63a22b00a704bee4b0254f4d"),
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Customer Number 1"
+  },
+  {
+    username: "customer2",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Customer Number 2"
+  },
+  {
+    username: "customer3",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Customer Number 3"
+  },
+  {
+    username: "customer4",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Customer Number 4"
+  },
+  {
+    username: "customer5",
+    password: "$2b$05$ADWgYlrf0.Szt/tJRZ4vt.Na/EIBIlpGYmM5y26GIAqErUuzZkfDi", // password
+    fullName: "Customer Number 5"
+  }
+  ],
+);
