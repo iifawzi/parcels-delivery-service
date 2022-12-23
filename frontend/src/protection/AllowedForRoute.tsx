917827d1ce752d6c9570c84e8d8e8913e5c3e628
iifawzi <iifawzie@gmail.com>
2022-12-23 17:55:09 +0200

    Tiny naming fix
    Signed-off-by: iifawzi <iifawzie@gmail.com>

--- frontend/src/protection/AllowedForRoute.tsx
+++ frontend/src/protection/AllowedForRoute.tsx
@@ -2,11 +2,11 @@ import { useAuth } from 'contexts/Auth.context';
 import { Navigate } from 'react-router-dom';
 import { UserInfoI } from 'types/auth/context.types';
 
-export const GuestRoute: React.FC<{ children: JSX.Element, role: string }> = ({ children, role }) => {
+export const AllowedForRoute: React.FC<{ children: JSX.Element, role: string }> = ({ children, role }) => {
     const { state } = useAuth()
     const userInfo = state.user as UserInfoI;
     return userInfo.role === role ? children : <Navigate to="/dashboard" />
 };
 
-export default GuestRoute;
+export default AllowedForRoute;
 
